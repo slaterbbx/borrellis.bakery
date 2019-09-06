0460848fd0988ae45fb70ef4a8872d929861454f
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-06 12:43:16 -0400

    added changes to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Borrelli's Bakery Website`,
+    description: `Borrelli's bakery in Providence Rhode island has been making baked goods since 1948`,
+    author: `@slaterbbx`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
