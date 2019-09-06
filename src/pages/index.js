cdf3ae0f70c133dbfb37a4abd1d13a7a1052a658
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-06 16:22:31 -0400

    small changes for an svg test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,13 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Icon from '../util/icons/Icons'
+import styled, {css} from 'styled-components'
+
+const Logo = css`
+	width: 600px;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -15,6 +22,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+		<Icon name="logo" styles={Logo}></Icon>
   </Layout>
 )
 
