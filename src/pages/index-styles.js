519c55c0cee56bf697d50b2fe4cbba04ce54adfa
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-06 23:42:13 -0400

    added styles for index page

---
+++ src/pages/index-styles.js
@@ -0,0 +1,40 @@
+import styled, {css} from 'styled-components'
+import media from '../util/mediaQuery'
+
+export const Logo = css`
+	position: relative;
+	display: block;
+	margin: 0 auto;
+	max-width: 90rem;
+
+	${media([null, 'max-840'], css`
+ 		max-height: 60rem;
+	`)}
+
+	${media([null, 'max-740'], css`
+ 		max-height: 50rem;
+	`)}
+
+	${media([null, 'max-540'], css`
+ 		max-height: 35rem;
+	`)}
+
+	${media([null, 'max-400'], css`
+ 		max-height: 30rem;
+	`)}
+
+	${media([null, 'max-340'], css`
+ 		max-height: 25rem;
+	`)}
+`
+
+export const SudoTable = styled.div`
+	display: table;
+	width: 100%;
+	height: 100%;
+
+	.sudoTableCell {
+		vertical-align: middle;
+		display: table-cell;
+	}
+`
