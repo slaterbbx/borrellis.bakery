131f226a10d4579b077bfc9284a0a4366e7e1a0a
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-07 04:59:34 -0400

    added x button

--- src/pages/index-styles.js
+++ src/pages/index-styles.js
@@ -2,21 +2,21 @@ import styled, { css } from "styled-components"
 import media from "../util/mediaQuery"
 
 export const Logo = css`
-	position: relative;
-	display: block;
-	margin: 0 auto;
-	max-width: 100%;
-	max-height: 90%;
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+  max-height: 90%;
 
-	${media(
-    [null, 'min-750'],
+  ${media(
+    [null, "min-750"],
     css`
       max-height: 70%;
     `
   )}
 
-${media(
-    ['min-400'],
+  ${media(
+    ["min-400"],
     css`
       max-width: 90%;
     `
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,23 @@ import Icon from "../util/icons/icons"
 import * as Styles from "./index-styles"
 
 import PageWrapper from "../components/pageWrapper/pageWrapper"
+import NavButton from '../components/navigation/navigation'
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <PageWrapper src="displayCase.jpg" innerGradiant={true}>
-      <Styles.SudoTable>
-        <div className="sudoTableCell">
-          <Icon name="logo" styles={Styles.Logo} />
-        </div>
-      </Styles.SudoTable>
-    </PageWrapper>
-  </>
-)
+const IndexPage = () => {
+  
+	return (
+		<>
+			<SEO title="Home" />
+			<PageWrapper src="displayCase.jpg" innerGradiant={true}>
+				<NavButton />
+				<Styles.SudoTable>
+					<div className="sudoTableCell">
+						<Icon name="logo" styles={Styles.Logo} />
+					</div>
+				</Styles.SudoTable>
+			</PageWrapper>
+		</>
+	)
+}
 
 export default IndexPage
