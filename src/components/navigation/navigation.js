9731f8fd9ad7143dd9ce25bd65cf68e8e8bddef3
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-07 23:24:32 -0400

    changed navigation slightly

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled, {css} from 'styled-components'
 import NavButton from './navigationButton'
+import Icon from '../../util/icons/icons'
 
 const MenuWrapper = styled.div`
 	position: absolute;
@@ -10,6 +11,7 @@ const MenuWrapper = styled.div`
 	width: 100%;
 	height: 100%;
 
+	overflow: hidden;
 	z-index: 2;
 
 	${({ active }) =>
@@ -25,6 +27,27 @@ const MenuWrapper = styled.div`
 			transition: .5s cubic-bezier(.25,0,.01,.99);
 			transform: translateY(100%);
     `}
+
+		.innerWrapper{
+			height: 100%;
+			width: 100%;
+
+			background: linear-gradient(
+				rgba(255, 255, 255, 0) 0%,
+				rgba(255, 255, 255, 0.1) 100%
+			);
+		}
+`
+
+const BreadIcon = css`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	color: white;
+	min-width: 150%;
+	min-height: 150%;
+	opacity: .03;
+	transform: translate(-50%, -43%) rotate(90deg);
 `
 
 const NavMenu = () => {
@@ -42,7 +65,11 @@ const NavMenu = () => {
 			clicked={buttonChangeHandler}/>
 
 			<MenuWrapper active={!buttonState[0]}
-			inactive={buttonState[0]}/>
+			inactive={buttonState[0]}>
+				<div className="innerWrapper">
+					<Icon name="bread" styles={BreadIcon} />
+				</div>
+			</MenuWrapper>
 		</>
 	)
 }
