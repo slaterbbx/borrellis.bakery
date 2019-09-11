151b5dfef067ff3536ff4bb612cc57dad6a2fc3b
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-11 19:40:15 -0400

    added a utility that I may use

---
+++ src/util/sudoTable.js
@@ -0,0 +1,27 @@
+import React from "react"
+import styled from "styled-components"
+
+const TableWrapper = styled.div`
+  display: table;
+  width: 100%;
+  height: 100%;
+
+  .sudoTableCell {
+    vertical-align: middle;
+    display: table-cell;
+
+    .cellCenter {
+      text-align: center;
+    }
+  }
+`
+
+const SudoTable = ({ children }) => (
+  <TableWrapper>
+    <div className="sudoTableCell">
+      <div className="cellCenter">{children}</div>
+    </div>
+  </TableWrapper>
+)
+
+export default SudoTable
