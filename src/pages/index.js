ca00175269da7e1598f77c984714beab0135c476
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-12 08:57:43 -0400

    brought remote css into local file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,37 @@
 import React from "react"
-// import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 
 import Icon from "../util/icons/icons"
-import * as Styles from "./index-styles"
+import { css } from 'styled-components'
 import PageWrapper from "../components/pageWrapper/pageWrapper"
 import Navigation from "../components/navigation/navigation"
 import SudoTable from "../util/sudoTable"
+import media from "../util/mediaQuery"
+
+export const Logo = css`
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+  max-height: 85vh;
+
+  z-index: 1;
+
+  ${media(
+    [null, "min-750"],
+    css`
+      max-height: 75vh;
+    `
+  )}
+
+  ${media(
+    ["min-400"],
+    css`
+      max-width: 90%;
+    `
+  )}
+`
 
 const IndexPage = () => {
   return (
@@ -16,7 +40,7 @@ const IndexPage = () => {
       <PageWrapper src="displayCase.jpg" innerGradiant={true}>
         <Navigation />
         <SudoTable>
-          <Icon name="logo" styles={Styles.Logo} />
+          <Icon name="logo" styles={Logo} />
         </SudoTable>
       </PageWrapper>
     </>
