5de8f33ec18e1693d24fc55b988a1ef1d43bc13c
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-12 05:49:43 -0400

    styles for custom gallery slider

---
+++ src/components/slideshow/slideShow-style.js
@@ -0,0 +1,71 @@
+import styled, { css } from 'styled-components'
+import media from '../../util/mediaQuery'
+
+export const SlideShowWrapper = styled.div`
+	position: absolute;
+	top: 15%;
+	left: 5rem;
+	transform: rotate(-20deg);
+	width: 55%;
+	height: 70%;
+
+	z-index: 20;
+
+	${media(
+		["min-2000"],
+		css`
+			left: 10rem;
+		`
+	)}
+`
+
+export const ArrowSharedStyles = css`
+	width: 100%;
+	margin-top: 2rem;
+	color: white;
+	transform: rotate(0deg);
+	transition: all .2s ease;
+
+	user-select: none;
+`
+export const ArrowRight = css`
+	${ArrowSharedStyles}
+
+	&:hover{
+		margin-top: 1.6rem;
+		filter: drop-shadow(0 .5rem .5rem rgb(0, 0, 0, 0.2));
+	}
+	
+`
+export const ArrowLeft = css`
+	${ArrowSharedStyles}
+	transform: rotate(180deg);
+
+	&:hover{
+		margin-top: 1.6rem;
+		filter: drop-shadow(0 .5rem .5rem rgb(0, 0, 0, 0.2));
+	}
+`
+export const ButtonWrapper = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`
+
+export const Button = styled.div`
+	width: 7rem;
+	cursor: pointer;
+	padding: .5rem;
+
+	&:not(:last-child){
+		margin-right: 3rem;
+	}
+`
+
+export const ImageWrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	border: solid 6rem white;
+	box-shadow: 2rem 2rem 10rem rgb(0,0,0,.2);
+
+	z-index: 5;
+`
