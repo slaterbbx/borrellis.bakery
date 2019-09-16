e9142267aff9b477ec4e596f40cdf4700ec5dad3
Kyle Gallagher <litonfiredesign@gmail.com>
2019-09-16 00:28:28 -0400

    added page background and navigation to global layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,52 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import { defaultTheme } from "../global/base-theme"
 import GlobalBaseStyles from "../global/base-styles"
 
-const Layout = ({ children }) => (
-  <ThemeProvider theme={defaultTheme}>
-    <>
-      <GlobalBaseStyles />
-      {children}
-    </>
-  </ThemeProvider>
-)
+import Navigation from '../components/navigation/navigation'
+import PageWrapper from '../components/pageWrapper/pageWrapper'
+
+const ContentWrapper = styled.div`
+	position: relative;
+  width: 100%;
+  height: 100vh;
+
+  overflow: hidden;
+`
+
+const Layout = ({ children, path }) => {
+
+	const background = useState('displayCase.jpg');
+
+	useEffect(()=>{
+		switch (path) {
+			case "/":
+				background[1]('displayCase.jpg')
+				break;
+			case "/menu/":
+				background[1]('bakery.jpg')
+				break;
+			default:
+				background[1]('displayCase.jpg')
+		}
+	},[background, path])
+
+	
+
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<>
+				<GlobalBaseStyles />
+				<ContentWrapper>
+					<Navigation />
+					<PageWrapper src={background[0]} innerGradiant={true}>
+						{children}
+					</PageWrapper>
+				</ContentWrapper>
+			</>
+		</ThemeProvider>
+	)
+}
 
 export default Layout
