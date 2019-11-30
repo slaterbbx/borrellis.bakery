1cabc40ade62a1a2a93b18e97060c726dcfd1c62
Kyle Gallagher <litonfiredesign@gmail.com>
2019-11-29 19:33:46 -0500

    changes for info animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Icon from '../util/icons/icons'
 
 const IndexPage = () => {
 
+	// need to setup global state to manage button activity values for animation functionality
 	let temp = true;
 
   return (
@@ -22,7 +23,6 @@ const IndexPage = () => {
 							<p className="copy">Established in 1948, Borrelli’s Bakery has been a Rhode Island staple for decades. We strive everyday to produce only the best products and service for every one of our customers.  We keep our traditional recipes alive with family values at the forefront.  From Italian bread and pastries to birthday cakes and catering, Borrelli's Bakery delivers the best tasting food with quality ingredients! For three generations, the Borrelli family has been handing down their traditional recipes and expertise to produce some of Rhode Island’s finest breads, pastries, pizzas, and more.</p>
 						</div>
 					</Styles.AboutWrapper>
-					{/* <Icon name="logo" styles={Styles.Logo}/> */}
     </>
   )
 }
