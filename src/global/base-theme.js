2b0cc944e35d37768db761e5c984cad1abbf5935
Kyle Gallagher <litonfiredesign@gmail.com>
2019-11-29 22:42:50 -0500

    added another theme color

--- src/global/base-theme.js
+++ src/global/base-theme.js
@@ -4,7 +4,8 @@ export const defaultTheme = {
   fontSecondary: "'source-sans-pro', sans-serif",
 
   colorBrandGreen: "#3D5525",
-  colorBrandRed: "#C93831",
+	colorBrandRed: "#C93831",
+	colorBrandRedLight: "#E23E37",
 
   colorDarkGrey: "#313131",
   colorDarkerGrey: "#212121",
