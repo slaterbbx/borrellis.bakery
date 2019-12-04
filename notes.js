403857a28f0d573d15a0b770556ef7549daff1db
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-03 23:40:18 -0500

    added notes file

---
+++ notes.js
@@ -0,0 +1,9 @@
+// FIX: home page background gradiant - remove from pageWrapper and put on index.js page
+
+// CREATE: Turn "about" wrapper and background into its own component for use on multiple pages 
+// FIX: Solve issue with opacity on 'about' wrapper so that inner font is positioned with z index so font is not effected.
+
+// CREATE: Finish 404 page ( requires about wrapper component completeion first )
+// CREATE: Menu page ( requires about wrapper component completeion first )
+// CREATE: Contact page ( requires hosting on Firebase first and using firebase cloud functions to email )
+// CREATE: Wholesale page 
