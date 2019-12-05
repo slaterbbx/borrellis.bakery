5b102d5255d5e91a0b78da62d9f53fb3e1f9a1a8
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-05 05:27:10 -0500

    style changes to index page

--- src/components/pageStyles/index-styles.js
+++ src/components/pageStyles/index-styles.js
@@ -1,4 +1,18 @@
-import styled, {css} from 'styled-components'
+import styled, {css, keyframes} from 'styled-components'
+
+const fadeIn = keyframes`
+
+	0% {
+			opacity: 0;
+			transform: translate(-50%, -53%) scale(.95);
+	}
+
+	100% {
+			opacity: 1;
+			transform: translate(-50%, -50%) scale(1);
+	}
+
+`
 
 export const InfoWrapper = styled.div`
 	position: relative;
@@ -20,13 +34,17 @@ export const LogoWrapper = styled.div`
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%) scale(.9);
+	padding: 0 2rem 0 2rem;
 	width: 100%;
-	height: 65vh;
+	height: 70vh;
 
 	opacity: 0;
 	transition: .9s all .3s cubic-bezier(0.25, 0, 0.01, 0.99);
 
 	z-index: 1;
+
+	animation: ${fadeIn} 1s ease-in;
+	animation-fill-mode: backwards;
 
 	${({ active }) =>
     active &&
