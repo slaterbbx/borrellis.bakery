caa65617f3e6b975f01eac6c940e67e9ab736acb
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-05 00:39:06 -0500

    updated index page to make use of new component

--- src/global/base-theme.js
+++ src/global/base-theme.js
@@ -1,7 +1,7 @@
 export const defaultTheme = {
   // FONTS
   fontPrimary: "'oxtail', serif",
-  fontSecondary: "'source-sans-pro', sans-serif",
+	fontSecondary: "'source-sans-pro', sans-serif",
 
   colorBrandGreen: "#3D5525",
 	colorBrandRed: "#C93831",
@@ -10,5 +10,5 @@ export const defaultTheme = {
   colorDarkGrey: "#313131",
   colorDarkerGrey: "#212121",
 
-  colorWhite: "#fff",
+	colorWhite: "#fff",
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import * as Styles from '../components/pageStyles/index-styles'
 import Icon from '../util/icons/icons'
 
 import {GlobalStateContext} from '../components/context/globalContext'
+import ContentBox from '../components/pageElements/contentBox/contentBox'
 
 
 const IndexPage = () => {
@@ -19,14 +20,11 @@ const IndexPage = () => {
 				<Styles.LogoWrapper active={infoButtonState}>
 					<Icon name="logo" styles={Styles.Logo}/>
 				</Styles.LogoWrapper>
-				<Styles.AboutWrapper active={infoButtonState}>
-					<div className="title">
-						About Borrelli's
-					</div>
-					<div className="innerWrapper">
-						<p className="copy">Established in 1948, Borrelli’s Bakery has been a Rhode Island staple for decades. We strive everyday to produce only the best products and service for every one of our customers.  We keep our traditional recipes alive with family values at the forefront.  From Italian bread and pastries to birthday cakes and catering, Borrelli's Bakery delivers the best tasting food with quality ingredients! For three generations, the Borrelli family has been handing down their traditional recipes and expertise to produce some of Rhode Island’s finest breads, pastries, pizzas, and more.</p>
-					</div>
-				</Styles.AboutWrapper>
+				<Styles.InfoWrapper active={infoButtonState}>
+					<ContentBox title="About Borrelli's" verticleCenter={true} opacity=".8">
+						Established in 1948, Borrelli’s Bakery has been a Rhode Island staple for decades. We strive everyday to produce only the best products and service for every one of our customers.  We keep our traditional recipes alive with family values at the forefront.  From Italian bread and pastries to birthday cakes and catering, Borrelli's Bakery delivers the best tasting food with quality ingredients! For three generations, the Borrelli family has been handing down their traditional recipes and expertise to produce some of Rhode Island’s finest breads, pastries, pizzas, and more.
+					</ContentBox>
+				</Styles.InfoWrapper>
     </>
   )
 }
