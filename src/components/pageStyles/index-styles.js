19941952ab54ba035994b73568735ee95f1bddec
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-05 00:31:37 -0500

    style changes to accomidate new contentBox component use

--- src/components/pageStyles/index-styles.js
+++ src/components/pageStyles/index-styles.js
@@ -1,12 +1,9 @@
 import styled, {css} from 'styled-components'
 
-export const AboutWrapper = styled.div`
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	width: 70%;
-
+export const InfoWrapper = styled.div`
+	position: relative;
+	width: 100%;
+	height: 100%;
 	transition: 0.9s all .3s cubic-bezier(0.25, 0, 0.01, 0.99);
 
   z-index: 1;
@@ -14,37 +11,8 @@ export const AboutWrapper = styled.div`
 	${({ active }) =>
     active &&
 		css`
-      transform: translate(-50%, -250%);
+      transform: translate(0, -250%);
     `}
-
-	.title{
-		display: block;
-		color: ${({theme})=> theme.colorWhite};
-		width: 100%;
-		font-size: 10rem;
-		line-height: .75;
-		opacity: .7;
-		margin-left: 1rem;
-		font-family: ${({theme}) => theme.fontPrimary};
-		text-align: left;
-	}
-	
-	.innerWrapper{
-		background-color: ${({theme}) => theme.colorWhite};
-		opacity: .7;
-		font-size: 5rem;
-		font-weight: 300;
-		padding: 7rem;
-		border-radius: 1rem;
-		font-family: ${({theme}) => theme.fontSecondary};
-
-		z-index: 1;
-	}
-
-	.copy{
-		font-size: 3.5rem;
-		font-weight: 300;
-	}
 `
 
 export const LogoWrapper = styled.div`
