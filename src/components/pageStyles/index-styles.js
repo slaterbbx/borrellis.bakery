40e7f71ed5683bc719c1d517370c353ede5121d3
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-05 20:37:17 -0500

    added styling

--- src/components/pageStyles/index-styles.js
+++ src/components/pageStyles/index-styles.js
@@ -18,13 +18,14 @@ export const InfoWrapper = styled.div`
 	position: relative;
 	width: 100%;
 	height: 100%;
-	transition: 0.9s all .3s cubic-bezier(0.25, 0, 0.01, 0.99);
+	transition: 0.7s all .3s ease-out;
 
   z-index: 1;
 
 	${({ active }) =>
     active &&
 		css`
+		/* moves contentBox off page */
       transform: translate(0, -250%);
     `}
 `
@@ -37,6 +38,8 @@ export const LogoWrapper = styled.div`
 	padding: 0 2rem 0 2rem;
 	width: 100%;
 	height: 70vh;
+
+	filter: drop-shadow( 0 2.5rem 1.5rem rgba(0, 0, 0, .28));
 
 	opacity: 0;
 	transition: .9s all .3s cubic-bezier(0.25, 0, 0.01, 0.99);
