0ab6c58c98f59e310d3f4c95ac6df09791f29ae7
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-07 07:02:27 -0500

    flipped state

--- src/components/context/globalContext.js
+++ src/components/context/globalContext.js
@@ -4,7 +4,7 @@ export const GlobalStateContext = React.createContext()
 export const GlobalDispatchContext = React.createContext()
 
 const initialState = {
-	menuButton: true,
+	menuButton: false,
 	infoButton: false,
 	isHomePage: false
 }
