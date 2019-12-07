f92bcefe8a9c1d5c7261bf47869b9992b73ccf76
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-07 04:54:26 -0500

    removed pageWrapper and moved to individual pages

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import styled, { ThemeProvider } from 'styled-components'
 import { defaultTheme } from '../global/base-theme'
 import GlobalBaseStyles from '../global/base-styles'
 import { HelmetProvider } from 'react-helmet-async';
 
-import Navigation from '../components/navigation/navigation'
-import PageWrapper from '../components/pageWrapper/pageWrapper'
-
 import GlobalContext from '../components/context/globalContext'
+import Navigation from '../components/navigation/navigation'
 
 const AppWrapper = styled.div`
 	position: relative;
@@ -21,53 +19,51 @@ const AppWrapper = styled.div`
 const Layout = ({ children, path }) => {
 
 	// Page background state ( changes with page address )
-	const background = useState('displayCase.jpg');
-	const gradiant = useState(true);
+	// const background = useState('displayCase.jpg');
+	// const gradiant = useState(true);
 
-	useEffect(()=>{
-		switch (path) {
-			case "/":
-				gradiant[1](false);
-				background[1]('case-bg.jpg');
-				break;
-			case "/menu/":
-				gradiant[1](false);
-				background[1]('bread.jpg');
-				break;
-			case "/about/":
-				gradiant[1](false);
-				background[1]('bakery.jpg');
-				break;
-			case "/contact/":
-				gradiant[1](false);
-				background[1]('treats.jpg');
-				break;
-			case "/wholesale/":
-				gradiant[1](false);
-				background[1]('carrotCake.jpg')
-				break;
-			default:
-				gradiant[1](true);
-				background[1]('displayCase.jpg')
-		}
-	// only runs on change of path props from react
-	// eslint-disable-next-line
-	},[path])
+	// useEffect(()=>{
+	// 	switch (path) {
+	// 		case "/":
+	// 			gradiant[1](false);
+	// 			background[1]('bread.jpg');
+	// 			break;
+	// 		case "/menu/":
+	// 			gradiant[1](false);
+	// 			background[1]('bread.jpg');
+	// 			break;
+	// 		case "/about/":
+	// 			gradiant[1](false);
+	// 			background[1]('bakery.jpg');
+	// 			break;
+	// 		case "/contact/":
+	// 			gradiant[1](false);
+	// 			background[1]('treats.jpg');
+	// 			break;
+	// 		case "/wholesale/":
+	// 			gradiant[1](false);
+	// 			background[1]('carrotCake.jpg')
+	// 			break;
+	// 		default:
+	// 			gradiant[1](true);
+	// 			background[1]('displayCase.jpg')
+	// 	}
+	// // only runs on change of path props from react
+	// // eslint-disable-next-line
+	// },[path])
 
 	return (
-			<ThemeProvider theme={defaultTheme}>
-				<GlobalContext>
-					<GlobalBaseStyles />
-					<AppWrapper>
-						<Navigation path={path}/>
-						<HelmetProvider>
-							<PageWrapper src={background[0]} gradiant={gradiant[0]}>
-								{children}
-							</PageWrapper>
-						</HelmetProvider>
-					</AppWrapper>
-				</GlobalContext>
-			</ThemeProvider>
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalContext>
+				<GlobalBaseStyles />
+				<AppWrapper>
+					<HelmetProvider>
+					<Navigation path={path}/>
+						{children}
+					</HelmetProvider>
+				</AppWrapper>
+			</GlobalContext>
+		</ThemeProvider>
 	)
 }
 
