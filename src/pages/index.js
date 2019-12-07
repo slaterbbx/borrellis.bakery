dc8576107d8b2aecb5e1cb857ef2e49d7af245f5
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-06 20:13:20 -0500

    updated to fix animation issues and positioning in firefox

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,14 @@ const IndexPage = () => {
   return (
     <>
       <SEO title="Home" />
-				<Styles.LogoWrapper active={infoButtonState}>
-					<Icon name="logo" styles={Styles.Logo}/>
-				</Styles.LogoWrapper>
+			{/* I had to put this here to break the flexboxes so that I could use relative positioning */}
+			<Styles.FloatFrame>
+				<Styles.VerticleCenter>
+					<Styles.LogoWrapper active={infoButtonState}>
+						<Icon name="logo" styles={Styles.Logo}/>
+					</Styles.LogoWrapper>
+				</Styles.VerticleCenter>
+			</Styles.FloatFrame>
 				<Styles.InfoWrapper active={infoButtonState}>
 					<ContentBox title="About Borrelli's" verticleCenter={true}>
 						Established in 1948, Borrelli’s Bakery has been a Rhode Island staple for decades. We strive everyday to produce only the best products and service for every one of our customers.  We keep our traditional recipes alive with family values at the forefront.  From Italian bread and pastries to birthday cakes and catering, Borrelli's Bakery delivers the best tasting food with quality ingredients! For three generations, the Borrelli family has been handing down their traditional recipes and expertise to produce some of Rhode Island’s finest breads, pastries, pizzas, and more.
