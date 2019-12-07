d323b561c9ecc09fbb75461cce30f7988143f271
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-06 20:12:45 -0500

    fixed glitch in mozilla

--- src/pageStyles/index-styles.js
+++ src/pageStyles/index-styles.js
@@ -4,23 +4,25 @@ const fadeIn = keyframes`
 
 	0% {
 			opacity: 0;
-			transform: translate(-50%, -50%) scale(.95);
+			transform: scale(.95);
 	}
 
 	100% {
 			opacity: 1;
-			transform: translate(-50%, -50%) scale(1);
+			transform: scale(1);
 	}
 
 `
 
 export const InfoWrapper = styled.div`
 	position: relative;
+	display: block;
+	transition: 0.7s all .3s ease-out;
+	transform: translate(0, 0);
 	width: 100%;
 	height: 100%;
-	transition: 0.7s all .3s ease-out;
 
-  z-index: 1;
+  z-index: 0;
 
 	${({ active }) =>
     active &&
@@ -30,13 +32,34 @@ export const InfoWrapper = styled.div`
     `}
 `
 
-export const LogoWrapper = styled.div`
+// Needed this to break the flex boxes for a verticle center
+export const FloatFrame = styled.div`
 	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
+	top: 0;
+	left: 0;
+	display: block;
+	width: 100%;
+	height: 100%;
+
+	z-index: 1;
+`
+
+export const VerticleCenter = styled.div`
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+`
+
+export const LogoWrapper = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
 	padding: 0 2rem 0 2rem;
-	height: 75vh;
+	width: 100%;
+	height: 70vh;
 
 	filter: drop-shadow( 0 2.5rem .6rem rgba(0, 0, 0, .5));
 
@@ -46,10 +69,10 @@ export const LogoWrapper = styled.div`
 	/* fixes glitch in firefox with animation  */
 	backface-visibility: hidden;
 
-	z-index: 1;
-
 	animation: ${fadeIn} 1s ease-in;
 	animation-fill-mode: backwards;
+
+	z-index: 1;
 
 	${({ active }) =>
     active &&
@@ -59,6 +82,7 @@ export const LogoWrapper = styled.div`
 `
 
 export const Logo = css`
+	min-width: 100%;
   max-width: 100%;
   max-height: 100%;
 `
