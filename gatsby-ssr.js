c89b67261e049fc19c9483bfce8c47636dbb5da8
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-07 21:39:33 -0500

    minor changes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,36 @@
  */
 
 // You can delete this file if you're not using it
+
+// const React = require('react');
+// const GlobalContextProvider = require('./src/components/context/globalContext').default;
+// const HelmetProvider = require('./src/components/context/globalContext').HelmetProvider;
+// const styled = require('styled-components');
+// const ThemeProvider = require('styled-components').ThemeProvider;
+// const GlobalBaseStyles = require('./src/global/base-styles');
+// const Navigation = require('./src/components/navigation/navigation');
+
+// const AppWrapper = styled.div`
+// 	position: relative;
+//   width: 100%;
+//   height: 100vh;
+
+//   overflow: hidden;
+// `
+
+// exports.wrapRootElement = ({ element }) => {
+// 	return (
+
+// 		<ThemeProvider theme={defaultTheme}>
+// 			<GlobalBaseStyles />
+// 			<AppWrapper>
+// 			{/* <Navigation/> */}
+// 				<GlobalContextProvider>
+// 					<HelmetProvider>
+// 						{element}
+// 					</HelmetProvider>
+// 				</GlobalContextProvider>
+// 			</AppWrapper>
+// 		</ThemeProvider>
+// 	);
+// }
