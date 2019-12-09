20e1fbd6d407d5b96c9e461a333ae9c3d506dd05
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-09 04:35:40 -0500

    changed design of the pagewrapper system

--- src/util/pageWrapper/pageWrapper.js
+++ src/util/pageWrapper/pageWrapper.js
@@ -2,11 +2,21 @@ import React from "react"
 import DynamicImage from '../dynamicImage/dynamicImage'
 import VerticleScroll from '../verticleScroll'
 
+import styled from 'styled-components'
+
+const GlobalWrapper = styled.div`
+	display: block;
+	width: 100%;
+	height: 100%;
+`
+
 const pageWrapper = ({src, tint, fadeAnim, children}) =>{
 	return(
 			<DynamicImage src={src} tint={tint} fadeAnim={fadeAnim}>
 				<VerticleScroll leftSide={true}>
-					{children}
+					<GlobalWrapper>
+							{children}
+					</GlobalWrapper>
 				</VerticleScroll>
 			</DynamicImage>
 	)
