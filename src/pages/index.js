273891ebc11454b54e52e90fd2186ec2c5d07208
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-09 05:18:29 -0500

    first official test of CD

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ const IndexPage = () => {
 					opacity=".6"
 					imgSrc="displayCase.jpg"
 					imgWidth="30%">
-						Established in 1948, Borrelli’s Bakery has been a Rhode Island staple for decades. We strive everyday to produce only the best products and service for every one of our customers.  We keep our traditional recipes alive with family values at the forefront.  From Italian bread and pastries to birthday cakes and catering, Borrelli's Bakery delivers the best tasting food with quality ingredients! For three generations, the Borrelli family has been handing down their traditional recipes and expertise to produce some of Rhode Island’s finest breads, pastries, pizzas, and more.
+						<p>( If this is here, CI / CD is working )Established in 1948, Borrelli’s Bakery has been a Rhode Island staple for decades. We strive everyday to produce only the best products and service for every one of our customers.  We keep our traditional recipes alive with family values at the forefront.  From Italian bread and pastries to birthday cakes and catering, Borrelli's Bakery delivers the best tasting food with quality ingredients! For three generations, the Borrelli family has been handing down their traditional recipes and expertise to produce some of Rhode Island’s finest breads, pastries, pizzas, and more.</p>
 					</ContentBox>
 				</InfoWrapper>
     </PageWrapper>
