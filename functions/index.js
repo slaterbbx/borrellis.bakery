159ce0f484d3602918debd01558245ee56345fe9
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-10 05:14:39 -0500

    removing firebase functions from the project but keeping code for examples

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,31 @@
 const functions = require('firebase-functions');
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const request = require('request');
+
+// // Automatically allow cross-origin requests
+// app.use(cors({ origin: true }));
+
+// app.get('/hello', (req, res) => {
+// 	// res.sendStatus(200)
+// 	// res.end("Received GET request!");
+// });
+
+// app.post('/hello', (req, res) => {
+// 	// res.sendStatus(200);
+// 	// res.end("Received POST request!");
+
+// 	// make the request
+// 	request('put your external url here', function (error, response, body) {
+// 		if (!error && response.statusCode == 200) {
+// 				//here put what you want to do with the request
+// 		}
+// 	})
+// });
+
+// // Expose Express API as a single Cloud Function:
+// exports.widgets = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
