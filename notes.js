c8cb1926ecdf9d49236149b930c27c323f570dae
Kyle Gallagher <litonfiredesign@gmail.com>
2019-12-10 00:34:24 -0500

    updated readme

--- notes.js
+++ notes.js
@@ -1,6 +1,8 @@
 // CLOUD FUNCTIONS
 // Capture POST from Prismic, convert data to own POST and send to Travis-ci for re-build and re-deployment
 
+// Get graphQl setup to pull from prismic on build time to populate the build with images and content from prismic
+
 
 // move infoButton on top of the nav menu and animate it.
 // tabindex="-1" 
