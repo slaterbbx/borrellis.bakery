49be6ca84e56065a9b46a1422e746ebf8c6c2dfc
Kyle Gallagher <litonfiredesign@gmail.com>
2020-02-29 23:06:36 -0500

    removed old code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,8 @@
-const { registerLinkResolver } = require('gatsby-source-prismic-graphql');
-const { linkResolver } = require('./src/util/prismicLinkResolver');
-
-registerLinkResolver(linkResolver);
-
-export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (typeof window.IntersectionObserver === `undefined`) {
-    import(`intersection-observer`)
-    console.log(`# IntersectionObserver is polyfilled!`)
-  }
-}
+// export const onClientEntry = () => {
+//   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+//   if (typeof window.IntersectionObserver === `undefined`) {
+//     import(`intersection-observer`)
+//     console.log(`# IntersectionObserver is polyfilled!`)
+//   }
+// }
 
